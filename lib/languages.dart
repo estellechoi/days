98cae78193bf6e0f2ed72ea57d4b7ad13fc2aa01
estellechoi <estele.choi@gmail.com>
2023-08-23 12:44:17 +0900

    chore: try to restrict key type

--- lib/languages.dart
+++ lib/languages.dart
@@ -1,18 +1,21 @@
 import 'package:get/get.dart';
 
-// typedef TranslationKey = 'greeting'; ??
+enum TranslationKey<String> {
+  greeting
+}
 
+// how to restrict the type of the key?
 class Languages extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
     'ko_KR': {
-      'greeting': '안녕하세요',
+      TranslationKey.greeting.toString(): '안녕하세요',
     },
     'ja_JP': {
-      'greeting': 'こんにちは',
+      TranslationKey.greeting.toString(): 'こんにちは',
     },
     'en_US': {
-      'greeting': 'Hello',
+      TranslationKey.greeting.toString(): 'Hello',
     },
   };
 }
